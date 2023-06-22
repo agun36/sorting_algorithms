1df4b90485a15bea88b9bd55aa3028bb9c4dc3d7
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-06-22 19:03:59 +0100

    Update partition.c

--- partition.c
+++ partition.c
@@ -12,7 +12,7 @@ int lomuto_partition(int *array, int low, int high)
 {
 	int pivot = array[high];
 	int i = low - 1;
-	int j;
+	int j, temp;
 
 	for (j = low; j <= high - 1; j++)
 	{
@@ -28,7 +28,7 @@ int lomuto_partition(int *array, int low, int high)
 		}
 	}
 	
-	int temp = array[i + 1];
+	temp = array[i + 1];
 	array[i + 1] = array[high];
 	array[high] = temp;
 
