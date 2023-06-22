9bd9a5d13aa930865e0152c355694ef436c0d3e7
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-06-22 18:05:52 +0100

    Update sort.h

--- sort.h
+++ sort.h
@@ -41,7 +41,7 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 /*Quick_sort Function*/
 void quick_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
+int lomuto_partition(int *array, int low, int high);
 void quicksort_recursive(int *array, int low, int high);
 /*swap function*/
 void swap(int *xp, int *yp);
