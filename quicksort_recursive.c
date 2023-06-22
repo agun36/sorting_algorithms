48a0c65c7b90bdef2d9d580ae38bae0f88ffa646
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-06-22 17:58:22 +0100

    Update quicksort_recursive.c

---
+++ quicksort_recursive.c
@@ -0,0 +1,16 @@
+/**
+ * quicksort_recursive - Recursive helper function for quick_sort
+ * @array: The array to be sorted
+ * @low: The starting index of the partition to be sorted
+ * @high: The ending index of the partition to be sorted
+ */
+void quicksort_recursive(int *array, int low, int high)
+{
+    if (low < high)
+    {
+        int pi = lomuto_partition(array, low, high);
+
+        quicksort_recursive(array, low, pi - 1);
+        quicksort_recursive(array, pi + 1, high);
+    }
+}
