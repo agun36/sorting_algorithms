3295fffb7a3729d6a58d228e11092c7595c603ce
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-06-22 15:58:39 +0100

    Update swap_nodes.c

--- swap_nodes.c
+++ swap_nodes.c
@@ -1,26 +1,22 @@
 #include "sort.h"
 /**
-  * swapNodes - contains two pointer with node1, node2
-  *
-  * Return: void
-  */
-void swapNodes(listint_t** node1, listint_t** node2)
+ * swapNodes - Swaps two nodes in a doubly linked list
+ * @list: Pointer to the head of the doubly linked list
+ * @node1: First node to swap
+ * @node2: Second node to swap
+ */
+void swapNodes(listint_t **list, listint_t *node1, listint_t *node2)
 {
-	listint_t* temp = *node1;
-	*node1 = *node2;
-	*node2 = temp;
+	if (node1->prev != NULL)
+		node1->prev->next = node2;
+	else
+		*list = node2;
 
-	if ((*node1)->prev != NULL)
-		(*node1)->prev->next = *node1;
+	if (node2->next != NULL)
+		node2->next->prev = node1;
 
-	if ((*node2)->next != NULL)
-		(*node2)->next->prev = *node2;
-
-	temp = (*node1)->prev;
-	(*node1)->prev = (*node2)->prev;
-	(*node2)->prev = temp;
-
-	temp = (*node1)->next;
-	(*node1)->next = (*node2)->next;
-	(*node2)->next = temp;
+	node1->next = node2->next;
+	node2->prev = node1->prev;
+	node1->prev = node2;
+	node2->next = node1;
 }
