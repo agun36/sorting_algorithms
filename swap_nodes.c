97c7c85098a1a64d960ee599dc0ee77d550feaba
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-06-22 16:08:46 +0100

    Update swap_nodes.c

--- swap_nodes.c
+++ swap_nodes.c
@@ -7,16 +7,20 @@
  */
 void swapNodes(listint_t **list, listint_t *node1, listint_t *node2)
 {
-    if (node1->prev != NULL)
-        node1->prev->next = node2;
-    else
-        *list = node2;
+	if (node1->prev != NULL)
+	{
+		node1->prev->next = node2;
+	}
+	else
+	{
+		*list = node2;
+	}
+	
+	if (node2->next != NULL)
+		node2->next->prev = node1;
 
-    if (node2->next != NULL)
-        node2->next->prev = node1;
-
-    node1->next = node2->next;
-    node2->prev = node1->prev;
-    node1->prev = node2;
-    node2->next = node1;
+	node1->next = node2->next;
+	node2->prev = node1->prev;
+	node1->prev = node2;
+	node2->next = node1;
 }
