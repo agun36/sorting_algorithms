057acd0a60d0fa8584e494e9ec3fd8317d30942d
agun36 <aguntoroti10@gmail.com>
2023-06-22 10:43:10 -0700

    main

--- partition.c
+++ partition.c
@@ -13,16 +13,25 @@ int lomuto_partition(int *array, int low, int high)
 	int pivot = array[high];
 	int i = low - 1;
 	int j;
-	
+
 	for (j = low; j <= high - 1; j++)
 	{
 		if (array[j] <= pivot)
-	{
-		i++;
-		if (i != j)
 		{
-			swap(&array[i], &array[j]);
-			print_array(array, high - low + 1);
+			i++;
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, high - low + 1);
+			}
 		}
-        }
-    }
+	}
+	
+	if (array[i + 1] != array[high])
+	{
+		swap(&array[i + 1], &array[high]);
+		print_array(array, high - low + 1);
+	}
+
+	return (i - 1);
+}
