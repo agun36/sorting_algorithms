9b7ae31bd0694e90544b48768d8df966482d6b4e
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-06-22 14:56:34 +0100

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,8 +9,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *sorted = NULL;
 	listint_t *current = *list;
-	listint_t *next;
-	listint_t *temp1, *temp2, *temp3;
+	listint_t *next = NULL;
 	
 	if (*list == NULL || (*list)->next == NULL)
 		return;
@@ -18,35 +17,23 @@ void insertion_sort_list(listint_t **list)
 	while (current != NULL)
 	{
 		next = current->next;
-		sorted = current->prev;
 
 		while (sorted != NULL && sorted->n > current->n)
 		{
-			temp1 = sorted->prev;
-			temp2 = current->next;
-			temp3 = current->prev;
-
-			if (sorted->prev != NULL)
-				sorted->prev->next = current;
-			if (current->next != NULL)
-				current->next->prev = sorted;
-
-			current->next = sorted;
-			current->prev = sorted->prev;
-			sorted->next = temp2;
-			sorted->prev = current;
-
-			sorted = temp1;
-			if (temp1 != NULL)
-				temp1->next = current;
-			if (temp2 != NULL)
-				temp2->prev = sorted;
-			if (temp3 != NULL)
-				temp3->next = sorted;
-			if (sorted == NULL)
-				*list = current;
-
+			swapNodes(&sorted, &current);
 			print_list(*list);
+		}
+
+		if (sorted == NULL)
+		{
+			*list = current;
+			sorted = current;
+		}
+		else
+		{
+			current->prev = sorted;
+			sorted->next = current;
+			sorted = current;
 		}
 
 		current = next;
