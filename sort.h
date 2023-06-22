48dd24d63ee65f45ae280ffc3fb335b83efbe642
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-06-22 11:28:42 +0100

    Update sort.h

--- sort.h
+++ sort.h
@@ -1,13 +1,11 @@
 #ifndef SORT_H
 #define SORT_H
 
-/* Header function */
 #include <stddef.h>
-#include <stdlib.h>
-#include <stdbool.h>
 #include "listint.h"
+#include "deck.h"
 
-/* Function prototypes */
+/* Sorting algorithms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
@@ -25,23 +23,5 @@ void sort_deck(deck_node_t **deck);
 /* Data structure and Functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
-
-/**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- */
-typedef struct listint_s
-{
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
-} listint_t;
-
-/* Swap function */
-void swap(int *xp, int *yp);
 
 #endif /* SORT_H */
