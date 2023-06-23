57b7af29f09e400802843f5aaeb6730cad672947
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-06-23 10:27:56 +0100

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,22 +1,6 @@
 #include "sort.h"
 
 /**
-  * swap - contains two pointers of integer xp and yp
-  *
-  *@xp: return pointer integer
-  *@yp: return second pointer integer
-  * Return: void
-  */
-void swap(int *xp, int *yp)
-{
-	int temp;
-
-	temp = *xp;
-	*xp = *yp;
-	*yp = temp;
-}
-
-/**
   * bubble_sort - sorts an array of integers in ascending order using
   * the Bubble Sort algorithm
   * @array: pointer to the array to be sorted
@@ -26,24 +10,17 @@ void swap(int *xp, int *yp)
   */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j = size;
-	bool swapped = false;
+	size_t i, j;
+	int temp;
 
-	if (array == NULL || size < 2)
-		return;
-
-	while (swapped == false)
-	{
-		swapped = true;
-		for (i = 0; i < j - 1; j++)
-		{
+	for (i = 0; i < size; i++)
+		for (j = 0; i < size - 1; j++)
 			if (array[i] > array[i + 1])
 			{
-				swap(array[i], array[i + 1]);
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 				print_array(array, size);
 				swapped = false;
 			}
-		}
-		j--;
-	}
 }
