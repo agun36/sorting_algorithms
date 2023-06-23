b1ae81910c3627132584316c1b689e7ac242addf
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-06-23 10:16:41 +0100

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -35,7 +35,7 @@ void bubble_sort(int *array, size_t size)
 	while (swapped == false)
 	{
 		swapped = true;
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
