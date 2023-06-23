3d74900c99388d634d0b81a6051c6b00cee4c113
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-06-23 09:35:19 +0100

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,4 +1,30 @@
 #include "sort.h"
+
+/**
+ * swapNodes - Swaps two nodes in a doubly linked list
+ * @node1: First node to swap
+ * @node2: Second node to swap
+ */
+void swapNodes(listint_t **list, listint_t *node1, listint_t *node2)
+{
+	if (node1->prev != NULL)
+	{
+		node1->prev->next = node2;
+	}
+	else
+	{
+		*list = node2;
+	}
+	
+	if (node2->next != NULL)
+		node2->next->prev = node1;
+
+	node1->next = node2->next;
+	node2->prev = node1->prev;
+	node1->prev = node2;
+	node2->next = node1;
+}
+
 /**
  * insertion_sort_list - Sorts a doubly linked list in ascending order
  * @list: Pointer to the head of the doubly linked list
