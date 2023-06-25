aedd6cd3a1e49a28f8b6ee4f330d0e82277d153a
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-06-25 14:41:55 +0100

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,22 +1,4 @@
 #include "sort.h"
-#include <stdio.h>
-
-/**
- * swap - contains two pointers of integer xp and yp
- *
- * @xp: return pointer integer
- * @yp: return second pointer integer
- * Return: void
- */
-void swap(int *xp, int *yp)
-{
-	int temp;
-
-	temp = *xp;
-	*xp = *yp;
-	*yp = temp;
-}
-
 /**
   * selection_sort - sort  array of integer in ascending order
   *
@@ -24,17 +6,24 @@ void swap(int *xp, int *yp)
   */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_idx;
+	size_t i, j, min_idx, temp;
 
-	for (i = 0; i < size-1; i++)
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
-		for (j = i+1; j < size; j++)
+		for (j = i + 1; j < size; j++)
+		{
 			if (array[j] < array[min_idx])
 				min_idx = j;
+		}
 		if (min_idx != i)
-			swap(&array[min_idx], &array[i]);
-		print_array(array, size);
+		{
+			temp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
+			print_array(array, size);
+		}
 	}
 }
-					
