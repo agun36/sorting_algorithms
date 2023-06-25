fa524c4787343cf0fc58d518988bced61a350427
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-06-25 18:32:53 +0100

    Update sort.h

--- sort.h
+++ sort.h
@@ -75,4 +75,9 @@ void merge_bit(int *array, size_t size, size_t low, size_t n,
 void bitonicSort(int *array, size_t size, size_t low, size_t n, char direction);
 void quick_sort_h(int *array, ssize_t low, ssize_t high, int size);
 int q_partition(int *array, int low, int high, int size);
+int _strcmp(const char *s1, const char *s2);
+char get_value(deck_node_t *card);
+void insertion_sort_deck_kind(deck_node_t **deck);
+void insertion_sort_deck_value(deck_node_t **deck);
+void sort_deck(deck_node_t **deck);
 #endif /* SORT_H */
