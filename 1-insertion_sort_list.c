622f1f436f39d5bffb607dcc6912efed514a1a6f
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-06-25 14:19:18 +0100

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,36 +2,31 @@
 
 /**
  * swapNodes - Swaps two nodes in a doubly linked list
+ * @head: A pointer to the head
  * @node1: First node to swap
  * @node2: Second node to swap
  */
-void swapNodes(listint_t **list, listint_t *node1, listint_t *node2)
+void swapNodes(listint_t **head, listint_t **node1, listint_t *node2)
 {
-	if (node1->prev != NULL)
-	{
-		node1->prev->next = node2;
-	}
-	else
-	{
-		*list = node2;
-	}
-	
+	(*node1)->next = node2->next;
 	if (node2->next != NULL)
-		node2->next->prev = node1;
-
-	node1->next = node2->next;
-	node2->prev = node1->prev;
-	node1->prev = node2;
-	node2->next = node1;
+		node2->next->prev = *node1;
+	node2->prev = (*node1)->prev;
+	node2->next = *node1;
+	if ((*node1)->prev != NULL)
+		(*node1)->prev->next = node2;
+	else
+		*head = node2;
+	(*node1)->prev = node2;
+	*node1 = node2->prev;
 }
-
 /**
  * insertion_sort_list - Sorts a doubly linked list in ascending order
  * @list: Pointer to the head of the doubly linked list
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *temp;
+	listint_t *current, *temp, insert;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
@@ -41,14 +36,14 @@ void insertion_sort_list(listint_t **list)
 	while (current != NULL)
 	{
 		temp = current->prev;
+		insert = current;
 
-		while (temp != NULL && temp->n > current->n)
+		while (temp != NULL && temp->n > insert->n)
 		{
-			swapNodes(list, temp, current);
+			swapNodes(list, &temp, insert);
 			print_list((const listint_t *)*list);
-			temp = current->prev;
+			temp = insert->prev;
 		}
-
 		current = current->next;
 	}
 }
