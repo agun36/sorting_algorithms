e7f96dc90f6198abfe97b7a7f0aeffca682c3dd9
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-06-26 10:04:05 +0100

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,13 +6,11 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, swap, swapped;
+	size_t i, j;
+	int swap;
 
-	if (array == NULL || size < 2)
-		return;
 	for (i = 0; i < size - 1; i++)
 	{
-		swapped = 0;
 		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -20,7 +18,6 @@ void bubble_sort(int *array, size_t size)
 				swap = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = swap;
-				swapped = 1;
 				print_array(array, size);
 			}
 		}
