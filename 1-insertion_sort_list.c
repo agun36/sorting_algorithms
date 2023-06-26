dc6dc521119f65051d3d74b0bdf6f628b2562d85
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-06-26 09:48:16 +0100

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,13 +6,13 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, swagged, temp;
+	size_t i, j, flag, temp;
 
 	if (array == NULL || size < 2)
 		return;
 	for (i = 0; i < size - 1; i++)
 	{
-		swagged = 0;
+		flag = 0;
 		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -20,11 +20,11 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				swagged = 1;
+				flag = 1;
 				print_array(array, size);
 			}
 		}
-		if (swagged == 0)
+		if (flag == 0)
 			break;
 	}
 }
