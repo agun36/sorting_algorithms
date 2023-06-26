630d97cbcdb229eb8b28c45bc6ea0aa02a96237a
agun36 <aguntoroti10@gmail.com>
2023-06-26 02:42:07 -0700

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -20,8 +20,8 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				print_array(array, size);
 				flag = 1;
+				print_array(array, size);
 			}
 		}
 		if (flag == 0)
