acf1dd1f7b89f031b213641c0dfb09b9de8da5ce
agun36 <aguntoroti10@gmail.com>
2023-06-26 02:26:36 -0700

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,15 +7,24 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int swap;
+	size_t flag;
 
 	for (i = 0; i < size - 1; i++)
+	{
+		flag = 0;
 		for (j = 0; j < size - 1 - i; j++)
+		{
 			if (array[j] > array[j + 1])
 			{
 				swap = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = swap;
 				print_array(array, size);
+				flag = 1;
 			}
+		}
+
+		if (flag == 0)
+			break;
+	}
 }
