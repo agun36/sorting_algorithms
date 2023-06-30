e26644d9c7db6eb4cc08122d6f9cd2453f15d5fb
agun36 <aguntoroti10@gmail.com>
2023-06-30 13:29:01 -0700

    main

---
+++ 0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+/**
+ * bubble_sort - sort array lements from min to max value
+ * @array: array
+ * @size: array size
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, flag, temp;
+
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		flag = 0;
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				flag = 1;
+				print_array(array, size);
+			}
+		}
+		if (flag == 0)
+			break;
+	}
+}
